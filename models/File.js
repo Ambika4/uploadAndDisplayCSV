3678e84b9efe40091d9e7840d847f5f23c458d35
Ambika Kumari <kambika1309@gmail.com>
2020-04-05 17:09:06 +0530

    added file schema and view and also installed multer for uploading file

---
+++ models/File.js
@@ -0,0 +1,40 @@
+const mongoose=require('mongoose');
+
+const multer=require('multer');
+const path=require('path');
+const AVATAR_PATH=path.join('/uploads/avatars');
+const fileSchema = new mongoose.Schema({
+  
+    //It store path of the uploads
+    avatar:{
+        type:String
+    }
+    },{
+        timestamps:true
+
+    });
+
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..', AVATAR_PATH));
+    },
+    //here file.fieldname is Avatar
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now());
+    }
+  });
+
+//static function
+//sngle function id for only one file will uploaded in avatar
+fileSchema.statics.uploadedAvatar=multer({storage: storage}).single('avatar');
+// as AVATAR_PATH need to publicly available
+fileSchema.statics.avatarPath=AVATAR_PATH;
+
+
+
+
+const File = mongoose.model('File',fileSchema);
+
+module.exports=File;
+
